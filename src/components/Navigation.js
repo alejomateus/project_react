d6a297f7233107128f0ba08308941daa966e038f
Alejandro Jimenez <alejo_mateus_@hotmail.com>
2018-10-23 21:30:41 -0500

    [DEV] First Componet with react

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,14 @@
-import React, { Component} from 'React';
+import React, { Component} from 'react';
 
 class Navigation extends Component{
-	
+	render(){
+		return(
+			<nav className="navbar navbar-dark bg-dark">
+				<a href="" className="text-white">
+					{this.props.title}
+				</a>
+			</nav>	
+		)
+	}
 }
+export default Navigation;
